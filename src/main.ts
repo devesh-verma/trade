97a4df1c9e54973a51e6783f2f4d2c965ec61790
Devesh Verma <deveshverma619@gmail.com>
2025-03-12 20:25:01 +0530

    [app]: enables validation pipe globally

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -7,6 +7,17 @@ const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable validation for all routes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  // Set up global prefix
+  app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
